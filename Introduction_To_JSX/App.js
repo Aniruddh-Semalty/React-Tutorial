b1b35a89b7eb766667f3e5548787d1baa80a0814
Aniruddh-Semalty <semaltyaniruddh001@gmail.com>
2024-01-23 13:58:49 +0530

    Used javascript inside a component and explored different ways to render componenent in react

--- Introduction_To_JSX/App.js
+++ Introduction_To_JSX/App.js
@@ -5,12 +5,19 @@ import ReactDOM from "react-dom/client";
 const Title=()=>(//shortform of react componenent
     <h2>This is another componenent and is rendered inside other componenent called component composition</h2>
 );
+const times=3;      //you can use javascript inside the component using {}.
+
 
 //converting JSX element into Componenent
 const HeadingComponent=()=>{
     return <div clssName="container">
-        <h1>Hello from functional component</h1>
-        <Title />
+        <h4>Here we are rendering our componenent in {times} different ways</h4>
+        
+        {Title()} {/* you can also render your component like this at the end of the day a component is a javascript function only*/}
+
+        <Title></Title> {/* you can also render componenet like this*/}
+        
+        <Title />  {/* Easiest way*/}
     </div>
 }
 //component is always rendered using </>syntax
