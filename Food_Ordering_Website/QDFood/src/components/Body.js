c7e5b943112dba203954f143fff6fa4772754c84
Aniruddh-Semalty <semaltyaniruddh001@gmail.com>
2024-01-28 15:30:38 +0530

    Added top rated and all restaurant filter buttons using useState Hook

--- Food_Ordering_Website/QDFood/src/components/Body.js
+++ Food_Ordering_Website/QDFood/src/components/Body.js
@@ -1,21 +1,48 @@
-import CardComponent from "./CardComponent"; 
-import restaurantList from "../utils/api_data";
- const Body = () => {
-    return (
-      <div className="body-container">
-        <div className="search-box">
-          <input
-            className="search-bar"
-            type="text"
-            placeholder="Type to search restaurants at your location"
-          />
+import { useState } from "react";
+import CardComponent from "./CardComponent";
+
+import resList from "../utils/api_data";
+
+const Body = () => {
+  const [restaurantList, setrestaurantList] = useState(resList);
+  return (
+    <div className="body-container">
+      <div className="search-box">
+        <input
+          className="search-bar"
+          type="text"
+          placeholder="Type to search restaurants at your location"
+        />
+        <div className="top-rated-btn-container">
+          <button
+            className="top-rated-btn"
+            onClick={() => {
+              const topRestaurants = restaurantList.filter((restaurant) => {
+                return restaurant.info.avgRating > 4.3;
+              });
+              setrestaurantList(topRestaurants);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
         </div>
-        <div className="restaurant-container">
-          {restaurantList.map((restaurant) => (
-            <CardComponent key={restaurant.info.id} restaurantObj={restaurant} />
-          ))}
+        <div>
+          <button
+            className="all-restaurants-btn"
+            onClick={() => {
+              setrestaurantList(resList);
+            }}
+          >
+            All Restaurants
+          </button>
         </div>
       </div>
-    );
-  };
-  export default Body;
+      <div className="restaurant-container">
+        {restaurantList.map((restaurant) => (
+          <CardComponent key={restaurant.info.id} restaurantObj={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Body;
